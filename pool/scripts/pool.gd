3c3f452c852d01a78b8ce1a267dfa1aaecb3083a
marineorganism <marineorganism@gmail.com>
2017-09-27 00:18:44 +1000

    more stuff

--- pool/scripts/pool.gd
+++ pool/scripts/pool.gd
@@ -1,16 +1,27 @@
 extends Node
 
 onready var panel = load("res://pool/objects/bar.tscn").instance()
+onready var banner = load("res://pool/objects/banner.tscn")
 onready var bar = panel.get_node("bar")
 onready var pissPen = get_node("piss")
 
 export var requirement = 50
+var winCount = -100
 
 func _ready():
 	set_process(true)
 	add_child(panel)
 
 func _process(delta):
-	bar.set_region_rect(Rect2(0,0,float(pissPen.get_child_count()) / requirement * 120,16))
+	var nPiss = float(pissPen.get_child_count())
+	bar.set_region_rect(Rect2(0,0,nPiss / requirement * 120,16))
+	if (nPiss > requirement && winCount == -100):
+		winCount = 5
+		var ib = banner.instance()
+		add_child(ib)
+	if (winCount > 0):
+		winCount -= delta
+	elif (winCount > -100):
+		print("end level")
 	
 	
